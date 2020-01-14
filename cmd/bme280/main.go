70c3ffdebad2b23f245836bfa9031db82117ee94
Charles Dixon <chvckd@gmail.com>
2020-01-14 19:36:15 +0000

    Init commit with support for testing bme280

---
+++ cmd/bme280/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/maciej/bme280"
+	"golang.org/x/exp/io/i2c"
+)
+
+const i2caddr = 0x76
+
+func main() {
+	device, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, i2caddr)
+	if err != nil {
+		panic(err)
+	}
+
+	driver := bme280.New(device)
+	err = driver.InitWith(bme280.ModeForced, bme280.Settings{
+		Filter:                  bme280.FilterOff,
+		Standby:                 bme280.StandByTime1000ms,
+		PressureOversampling:    bme280.Oversampling16x,
+		TemperatureOversampling: bme280.Oversampling16x,
+		HumidityOversampling:    bme280.Oversampling16x,
+	})
+
+	response, err := driver.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Humidity: %f, Temperature: %f, Pressure: %f\n", response.Humidity, response.Temperature,
+		response.Pressure)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/chvck/weather-station
+
+go 1.13
+
+require (
+	github.com/maciej/bme280 v0.2.0
+	golang.org/x/exp v0.0.0-20191227195350-da58074b4299
+)
